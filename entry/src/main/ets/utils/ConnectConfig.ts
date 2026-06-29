4a3d4231fb74773cf8d1a3665db1e3193f37b157
2285-qw <74612972+2285-qw@users.noreply.github.com>
2026-06-29 10:15:31 +0800

    第一次上传代码

---
+++ entry/src/main/ets/utils/ConnectConfig.ts
@@ -0,0 +1,8 @@
+// utils/GlobalConfig.ts（单独文件）
+export class ConnectConfig {
+  static TF_CARD_REMAIN_SPACE: number = 0; //tf卡剩余容量
+  static TF_CARD_TOTAL_SPACE: number = 0; //tf卡总容量
+  static IS_CROSS: boolean = false
+  static IS_WELL: boolean = false
+  static IS_PLUS_SIGN: boolean = false
+}
